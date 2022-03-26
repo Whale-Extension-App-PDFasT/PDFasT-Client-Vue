5c95a483e5232905e4bd304e2127cb018a6e59d4
taeuk <taeuk@taeukui-MacBookPro.local>
2022-03-26 21:38:16 +0900

    update manifest 4.0.0

--- src/manifest.js
+++ src/manifest.js
@@ -1,18 +1,12 @@
-
 module.exports = {
   name: '__MSG_APP_NAME__',
-  version: '3.0.2',
+  version: '4.0.0',
   description: '__MSG_APP_DESCRIPTION__',
   author: 'KANG TAE UK, MOON YONG HO, JEON GA BIN',
   default_locale: 'ko',
   manifest_version: 2,
   icons: { '128': 'icons/128.png' },
-  permissions: [
-    '<all_urls>',
-    '*://*/*',
-    'activeTab',
-    'tabs'
-  ],
+  permissions: ['<all_urls>', '*://*/*', 'activeTab', 'tabs'],
   sidebar_action: {
     default_title: '__MSG_APP_NAME__',
     default_page: 'pages/popup.html',
